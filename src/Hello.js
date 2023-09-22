5dc3ffd53fa7aab32bcad0eac484a4e64236d0ab
kimin <kiminchan12341@gmail.com>
2023-09-22 11:30:39 +0900

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div className="Hello">
+                <lable for="hello">안녕하세요!</lable>
+            </div>
+        );
+    }
+}
+
+export default Hello;
